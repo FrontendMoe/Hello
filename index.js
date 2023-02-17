3ae79391d4bcf851024e0c0da36efefad456369a
Mohamed <mboukachabia13@gmail.com>
2023-02-17 16:06:42 +0100

    changes

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-const express = require("express");
-
+const express = require('express');
+const ChatGPTAPI = require('chatgpt').ChatGPTAPI;
+const api = new ChatGPTAPI({
+	apiKey: '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',
+});
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+app.get('/', async (req, res) => {
+	const result = await api.sendMessage('hello');
+	res.send(result);
 });
 
 app.listen(5000, () => {
-  console.log("Running on port 5000.");
+	console.log('Running on port 5000.');
 });
 
 // Export the Express API
