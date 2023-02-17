4929031b7841c76dec80de39b52795583e3e7350
Mohamed <mboukachabia13@gmail.com>
2023-02-17 16:15:56 +0100

    changes

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-const express = require('express');
-const ChatGPTAPI = require('chatgpt').ChatGPTAPI;
+import Express from 'express';
+import { ChatGPTAPI } from 'chatgpt';
 const api = new ChatGPTAPI({
 	apiKey: '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',
 });
-const app = express();
+const app = Express();
 
 app.get('/', async (req, res) => {
 	const result = await api.sendMessage('hello');
-	res.send(result);
+	res.send(result + 'hhhh');
 });
 
 app.listen(5000, () => {
 	console.log('Running on port 5000.');
 });
-
-// Export the Express API
-module.exports = app;
